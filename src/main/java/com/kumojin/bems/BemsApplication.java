b7cb121120e9b61d51ea2774486b16643b634fca
benji62f <benji62f@hotmail.fr>
2022-07-22 22:08:56 +0200

    Spring boot Initializr

---
+++ src/main/java/com/kumojin/bems/BemsApplication.java
@@ -0,0 +1,13 @@
+package com.kumojin.bems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kumojin/bems/BemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumojin.bems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
